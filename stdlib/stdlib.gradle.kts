b1ff2ecf0f9faaf98e8edc1793b0e2af188fcf23
Philip Hölzenspies <pholzenspies@apple.com>
2024-02-05 11:11:57 +0000

    Remove lingering, outdated build target `pkldoc`

--- stdlib/stdlib.gradle.kts
+++ stdlib/stdlib.gradle.kts
@@ -7,12 +7,6 @@ plugins {
   signing
 }
 
-val pkldocConfiguration: Configuration = configurations.create("pkldoc")
-
-dependencies {
-  pkldocConfiguration(project(":pkl-doc"))
-}
-
 // create and publish a self-contained stdlib archive
 // purpose is to provide non-jvm tools/projects with a versioned stdlib
 val stdlibZip by tasks.registering(Zip::class) {
@@ -49,27 +43,5 @@ spotless {
   format("pkl") {
     target("*.pkl")
     licenseHeaderFile(rootProject.file("buildSrc/src/main/resources/license-header.line-comment.txt"), "/// ")
-  }
-}
-
-val pkldoc by tasks.registering(JavaExec::class) {
-  val stdlibFiles = project.fileTree(projectDir).matching {
-    include("*.pkl")
-    exclude("doc-package-info.pkl")
-  }
-  val infoFiles = project.files("doc-package-info.pkl")
-
-  inputs.files(stdlibFiles)
-  inputs.files(infoFiles)
-  outputs.dir("$buildDir/pkldoc")
-
-  classpath = pkldocConfiguration
-  main = "org.pkl.doc.Main"
-  args("--output-dir", "$buildDir/pkldoc")
-  args(stdlibFiles.map { "pkl:${it.name}".dropLast(4) })
-  args(infoFiles)
-
-  doLast {
-    println("Generated Standard Library API Docs at: file://$buildDir/pkldoc/index.html")
   }
 }
