2321319b02938f73ae775539ad1eaeba7d75538d
translatenix <119817707+translatenix@users.noreply.github.com>
2024-02-14 06:14:21 -0800

    Add Gradle task "testNative"
    Support running native tests with "gw testNative" for consistency with "gw test".

--- buildSrc/src/main/kotlin/pklNativeBuild.gradle.kts
+++ buildSrc/src/main/kotlin/pklNativeBuild.gradle.kts
@@ -1,6 +1,10 @@
 val assembleNative by tasks.registering {}
 
-val checkNative by tasks.registering {}
+val testNative by tasks.registering {}
+
+val checkNative by tasks.registering {
+  dependsOn(testNative)
+}
 
 val buildNative by tasks.registering {
   dependsOn(assembleNative, checkNative)
