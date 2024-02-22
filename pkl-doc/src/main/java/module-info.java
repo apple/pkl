a510e40d888f317af6f62149b3054fcdbc22e4e7
Sam Gammon <sam@elide.ventures>
2024-02-21 20:51:54 -0800

    feat: jpms for `pkl-doc`
    Signed-off-by: Sam Gammon <sam@elide.ventures>

---
+++ pkl-doc/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * Pkl: Doc.
+ */
+module pkl.doc {
+  requires java.base;
+  requires kotlin.stdlib;
+
+  requires kotlinx.serialization.core;
+  requires kotlinx.serialization.json;
+
+  requires pkl.core;
+  requires pkl.commons;
+  requires pkl.commons.cli;
+}
