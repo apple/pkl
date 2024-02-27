09be2f416d4444cdef6670926e258826b7d1f211
translatenix <119817707+translatenix@users.noreply.github.com>
2024-02-26 18:26:36 -0800

    Publish pkl-certs JAR

--- pkl-certs/pkl-certs.gradle.kts
+++ pkl-certs/pkl-certs.gradle.kts
@@ -1,4 +1,19 @@
 plugins {
   pklAllProjects
   pklJavaLibrary
+  pklPublishLibrary
+}
+
+publishing {
+  publications {
+    named<MavenPublication>("library") {
+      pom {
+        url.set("https://github.com/apple/pkl/tree/main/pkl-certs")
+        description.set("""
+          Pkl's built-in CA certificates. 
+          Used by pkl-cli and optionally supported by pkl-core.")
+        """.trimIndent())
+      }
+    }
+  }
 }
