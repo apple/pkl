8338c17e6eb496616d659f2be46d05fdd4d2050f
StefMa <StefMaDev@outlook.com>
2024-04-03 13:06:14 +0200

    Remove unnecessary conversion

--- buildSrc/src/main/kotlin/JvmToolchain.kt
+++ buildSrc/src/main/kotlin/JvmToolchain.kt
@@ -1,12 +1,11 @@
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.jvm.toolchain.JvmVendorSpec
-import org.gradle.api.Project
 
 private const val JDK_VERSION = 11
 
 val jvmToolchainVersion: JavaLanguageVersion
     get() {
-        val jvmVersion = System.getProperty("PKL_JVM_VERSION", JDK_VERSION.toString()).toInt()
+        val jvmVersion = System.getProperty("PKL_JVM_VERSION")?.toInt() ?: JDK_VERSION
         return JavaLanguageVersion.of(jvmVersion)
     }
 
