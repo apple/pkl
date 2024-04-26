2f900963f620e2250b40fc7c0462d6d9c2fdfea1
translatenix <119817707+translatenix@users.noreply.github.com>
2024-04-26 15:08:28 -0700

    Remove obsolete Alpine Linux workaround
    According to https://github.com/oracle/graal/issues/3398,
    the bug necessitating this workaround was fixed in October 2022.

--- pkl-cli/src/main/kotlin/org/pkl/cli/Main.kt
+++ pkl-cli/src/main/kotlin/org/pkl/cli/Main.kt
@@ -19,37 +19,23 @@ package org.pkl.cli
 
 import com.github.ajalt.clikt.core.subcommands
 import org.pkl.cli.commands.*
-import org.pkl.commons.cli.CliMain
 import org.pkl.commons.cli.cliMain
 import org.pkl.core.Release
 
 /** Main method of the Pkl CLI (command-line evaluator and REPL). */
 internal fun main(args: Array<String>) {
-  val version = Release.current().versionInfo()
-  val helpLink = "${Release.current().documentation().homepage()}pkl-cli/index.html#usage"
-  val commands =
-    arrayOf(
-      EvalCommand(helpLink),
-      ReplCommand(helpLink),
-      ServerCommand(helpLink),
-      TestCommand(helpLink),
-      ProjectCommand(helpLink),
-      DownloadPackageCommand(helpLink)
-    )
-  val cmd = RootCommand("pkl", version, helpLink).subcommands(*commands)
   cliMain {
-    if (CliMain.compat == "alpine") {
-      // Alpine's main thread has a prohibitively small stack size by default;
-      // https://github.com/oracle/graal/issues/3398
-      var throwable: Throwable? = null
-      Thread(null, { cmd.main(args) }, "alpineMain", 10000000).apply {
-        setUncaughtExceptionHandler { _, t -> throwable = t }
-        start()
-        join()
-      }
-      throwable?.let { throw it }
-    } else {
-      cmd.main(args)
-    }
+    val version = Release.current().versionInfo()
+    val helpLink = "${Release.current().documentation().homepage()}pkl-cli/index.html#usage"
+    RootCommand("pkl", version, helpLink)
+      .subcommands(
+        EvalCommand(helpLink),
+        ReplCommand(helpLink),
+        ServerCommand(helpLink),
+        TestCommand(helpLink),
+        ProjectCommand(helpLink),
+        DownloadPackageCommand(helpLink)
+      )
+      .main(args)
   }
 }
