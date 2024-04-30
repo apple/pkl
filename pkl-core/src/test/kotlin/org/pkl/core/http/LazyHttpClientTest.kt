e4b5823fee0626a991b3dd22a841a7921c3e9f32
translatenix <119817707+translatenix@users.noreply.github.com>
2024-04-30 10:50:14 -0700

    Fix test

--- pkl-core/src/test/kotlin/org/pkl/core/http/LazyHttpClientTest.kt
+++ pkl-core/src/test/kotlin/org/pkl/core/http/LazyHttpClientTest.kt
@@ -3,15 +3,20 @@ package org.pkl.core.http
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.io.TempDir
+import org.pkl.commons.createTempFile
+import org.pkl.commons.writeString
 import java.net.URI
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers
+import java.nio.file.Path
 
 class LazyHttpClientTest {
   @Test
-  fun `builds underlying client on first send`() {
+  fun `builds underlying client on first send`(@TempDir tempDir: Path) {
+    val certFile = tempDir.resolve("cert.pem").apply { writeString("broken") }
     val client = HttpClient.builder()
-      .addCertificates(javaClass.getResource("brokenCerts.pem")!!.toURI())
+      .addCertificates(certFile)
       .buildLazily()
     val request = HttpRequest.newBuilder(URI("https://example.com")).build()
     
@@ -21,9 +26,10 @@ class LazyHttpClientTest {
   }
   
   @Test
-  fun `does not build underlying client unnecessarily`() {
+  fun `does not build underlying client unnecessarily`(@TempDir tempDir: Path) {
+    val certFile = tempDir.createTempFile().apply { writeString("broken") }
     val client = HttpClient.builder()
-      .addCertificates(javaClass.getResource("brokenCerts.pem")!!.toURI())
+      .addCertificates(certFile)
       .buildLazily()
     
     assertDoesNotThrow {
