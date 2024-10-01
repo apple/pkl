9c817bc24e7927411bbaa9d68bd269690dfba7cc
Philip K.F. Hölzenspies <pholzenspies@apple.com>
2024-10-01 16:13:38 +0100

    Fix test cases

--- pkl-core/src/test/kotlin/org/pkl/core/project/ProjectDependenciesResolverTest.kt
+++ pkl-core/src/test/kotlin/org/pkl/core/project/ProjectDependenciesResolverTest.kt
@@ -86,6 +86,7 @@ class ProjectDependenciesResolverTest {
           }
         }
       }
+      
     """
           .trimIndent()
       )
--- pkl-core/src/test/kotlin/org/pkl/core/project/ProjectDepsTest.kt
+++ pkl-core/src/test/kotlin/org/pkl/core/project/ProjectDepsTest.kt
@@ -44,6 +44,7 @@ class ProjectDepsTest {
             }
           }
         }
+        
       """
       .trimIndent()
 
