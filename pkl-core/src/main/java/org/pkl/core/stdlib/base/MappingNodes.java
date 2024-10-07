a0a74e840140c2bc1e647345db31219fa8680b1e
Philip K.F. Hölzenspies <holzensp@gmail.com>
2024-10-07 17:08:36 +0100

    Add `entries` to `Mapping`

--- pkl-core/src/main/java/org/pkl/core/stdlib/base/MappingNodes.java
+++ pkl-core/src/main/java/org/pkl/core/stdlib/base/MappingNodes.java
@@ -84,6 +84,17 @@ public final class MappingNodes {
     }
   }
 
+  public abstract static class entries extends ExternalPropertyNode {
+    @Specialization
+    protected VmList eval(VmMapping self) {
+      var builder = VmList.newBuilder();
+      for (var key : self.getAllKeys()) {
+        builder.add(new VmPair(key, VmUtils.readMember(self, key)));
+      }
+      return builder.build();
+    }
+  }
+
   public abstract static class containsKey extends ExternalMethod1Node {
     @Specialization
     protected boolean eval(VmMapping self, Object key) {
