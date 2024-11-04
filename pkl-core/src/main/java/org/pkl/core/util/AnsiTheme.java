9ad4ec79f013a30204e7218e1b2510b05c6a595a
Dan Chao <dan.chao@apple.com>
2024-11-04 13:28:46 -0800

    Run spotless apply

--- pkl-core/src/main/java/org/pkl/core/util/AnsiTheme.java
+++ pkl-core/src/main/java/org/pkl/core/util/AnsiTheme.java
@@ -22,20 +22,20 @@ import org.pkl.core.runtime.AnsiCodingStringBuilder.AnsiCode;
 public final class AnsiTheme {
   private AnsiTheme() {}
 
-  public final static AnsiCode ERROR_MESSAGE_HINT = AnsiCode.YELLOW;
-  public final static AnsiCode ERROR_HEADER = AnsiCode.RED;
-  public final static Set<AnsiCode> ERROR_MESSAGE = EnumSet.of(AnsiCode.RED, AnsiCode.BOLD);
+  public static final AnsiCode ERROR_MESSAGE_HINT = AnsiCode.YELLOW;
+  public static final AnsiCode ERROR_HEADER = AnsiCode.RED;
+  public static final Set<AnsiCode> ERROR_MESSAGE = EnumSet.of(AnsiCode.RED, AnsiCode.BOLD);
 
-  public final static AnsiCode STACK_FRAME = AnsiCode.FAINT;
-  public final static AnsiCode STACK_TRACE_MARGIN = AnsiCode.YELLOW;
-  public final static AnsiCode STACK_TRACE_LINE_NUMBER = AnsiCode.BLUE;
-  public final static AnsiCode STACK_TRACE_LOOP_COUNT = AnsiCode.MAGENTA;
-  public final static AnsiCode STACK_TRACE_CARET = AnsiCode.RED;
+  public static final AnsiCode STACK_FRAME = AnsiCode.FAINT;
+  public static final AnsiCode STACK_TRACE_MARGIN = AnsiCode.YELLOW;
+  public static final AnsiCode STACK_TRACE_LINE_NUMBER = AnsiCode.BLUE;
+  public static final AnsiCode STACK_TRACE_LOOP_COUNT = AnsiCode.MAGENTA;
+  public static final AnsiCode STACK_TRACE_CARET = AnsiCode.RED;
 
-  public final static AnsiCode FAILING_TEST_MARK = AnsiCode.RED;
-  public final static AnsiCode PASSING_TEST_MARK = AnsiCode.GREEN;
-  public final static AnsiCode TEST_NAME = AnsiCode.FAINT;
-  public final static AnsiCode TEST_FACT_SOURCE = AnsiCode.RED;
-  public final static AnsiCode TEST_FAILURE_MESSAGE = AnsiCode.RED;
-  public final static Set<AnsiCode> TEST_EXAMPLE_OUTPUT = EnumSet.of(AnsiCode.RED, AnsiCode.BOLD);
+  public static final AnsiCode FAILING_TEST_MARK = AnsiCode.RED;
+  public static final AnsiCode PASSING_TEST_MARK = AnsiCode.GREEN;
+  public static final AnsiCode TEST_NAME = AnsiCode.FAINT;
+  public static final AnsiCode TEST_FACT_SOURCE = AnsiCode.RED;
+  public static final AnsiCode TEST_FAILURE_MESSAGE = AnsiCode.RED;
+  public static final Set<AnsiCode> TEST_EXAMPLE_OUTPUT = EnumSet.of(AnsiCode.RED, AnsiCode.BOLD);
 }
