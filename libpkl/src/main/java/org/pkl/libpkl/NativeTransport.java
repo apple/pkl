692fe4b4363bbe213f0496289dc9ffd6940ba733
Kushal Pisavadia <kushal.p@apple.com>
2025-03-18 17:49:41 +0000

    C library for Pkl
    This introduces native C bindings for Pkl.
    
    Dynamic Library
    ---------------
    
    Using `org.graalvm.nativeimage` and the `native-image` binary we
    produce a dynamic library (for each OS/Arch variant) that provides a
    way to initialise itself with a `graal_isolatethread_t`.
    
    Methods are annotated with `@CEntryPoint` and exported.
    
    This change results in an architecture and OS-specific directory being
    created which now produces the headers for our shared library
    functions:
    
    ```
    ❯ ll libpkl/build/libs/macos-aarch64/
    
    graal_isolate_dynamic.h
    graal_isolate.h
    libpkl-internal_dynamic.h
    libpkl-internal-macos-aarch64_dynamic.h
    libpkl-internal-macos-aarch64.dylib
    libpkl-internal-macos-aarch64.h
    libpkl-internal.dylib
    libpkl-internal.h
    ```
    
    `libpkl`
    --------
    
    The produced `libpkl` dynamic library wraps the GraalVM C interface
    into something that is future-friendly for the needs of a Pkl
    integrator. It exports an interface which aligns with SPICE-0015[1].
    
    ```
    ❯ ll libpkl/build/libs/macos-aarch64/
    
    graal_isolate_dynamic.h
    graal_isolate.h
    libpkl-internal_dynamic.h
    libpkl-internal-macos-aarch64_dynamic.h
    libpkl-internal-macos-aarch64.dylib
    libpkl-internal-macos-aarch64.h
    libpkl-internal.dylib
    libpkl-internal.h
    libpkl.dylib    <--- this is new
    libpkl.h        <--- this is new
    ```
    
    JNA
    ---
    
    Testing of the produced `libpkl` dynamic library is done using Java
    Native Access[2] for ease. We provide an `interface` in Kotlin which
    JNA transposes against the `libpkl` dynamic library discoverable at
    the path that is discoverable with `jna.library.path`.
    
    Load in `projects.pklCommonsCli` to deal with `UnsupportedFeatureException`
    ---------------------------------------------------------------------------
    
    This is to deal with the following error:
    
    ```
    Caused by: com.oracle.graal.pointsto.constraints.UnsupportedFeatureException: Detected a started Thread in the image heap. Thread name: main. Threads running in the image generator are no longer running at image runtime. If these objects should not be stored in the image heap, you can use
    
        '--trace-object-instantiation=java.lang.Thread'
    ```
    
    [1] https://github.com/apple/pkl-evolution/pull/16

---
+++ libpkl/src/main/java/org/pkl/libpkl/NativeTransport.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright © 2025 Apple Inc. and the Pkl project authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.pkl.libpkl;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.function.Consumer;
+import org.graalvm.nativeimage.c.type.CCharPointer;
+import org.msgpack.core.MessagePack;
+import org.pkl.core.messaging.Message;
+import org.pkl.core.messaging.MessageTransports.AbstractMessageTransport;
+import org.pkl.core.messaging.MessageTransports.Logger;
+import org.pkl.core.messaging.ProtocolException;
+import org.pkl.server.ServerMessagePackDecoder;
+import org.pkl.server.ServerMessagePackEncoder;
+
+public class NativeTransport extends AbstractMessageTransport {
+  private final Consumer<byte[]> sendMessageToNative;
+
+  protected NativeTransport(Logger logger, Consumer<byte[]> sendMessageToNative) {
+    super(logger);
+    this.sendMessageToNative = sendMessageToNative;
+  }
+
+  @Override
+  protected void doStart() throws ProtocolException, IOException {}
+
+  @Override
+  protected void doClose() {}
+
+  @Override
+  protected void doSend(Message message) throws IOException, ProtocolException {
+    try (var os = new ByteArrayOutputStream();
+        var packer = MessagePack.newDefaultPacker(os)) {
+      var encoder = new ServerMessagePackEncoder(packer);
+      encoder.encode(message);
+      sendMessageToNative.accept(os.toByteArray());
+    }
+  }
+
+  public void sendMessage(int length, CCharPointer ptr) throws IOException, ProtocolException {
+    try (var is = new NativeInputStream(length, ptr);
+        var unpacker = MessagePack.newDefaultUnpacker(is)) {
+      var message = new ServerMessagePackDecoder(unpacker).decode();
+      assert message != null;
+      accept(message);
+    }
+  }
+}
---
+++ libpkl/src/test/kotlin/org/pkl/libpkl/ILibPklLibrary.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2025 Apple Inc. and the Pkl project authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.pkl.libpkl
+
+import com.sun.jna.Callback
+import com.sun.jna.Library
+import com.sun.jna.Native
+import com.sun.jna.Pointer
+
+interface ILibPklLibrary : Library {
+  companion object {
+    val INSTANCE: ILibPklLibrary = Native.load("pkl", ILibPklLibrary::class.java)
+  }
+
+  interface PklMessageResponseHandler : Callback {
+    fun invoke(length: Int, message: Pointer?)
+  }
+
+  fun pkl_init(handler: PklMessageResponseHandler?): Int
+
+  fun pkl_send_message(length: Int, message: ByteArray?): Int
+
+  fun pkl_close(): Int
+}
