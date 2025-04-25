b5f42604c9f23fe856212949bc0d6949ec74c9dc
Artem Yarmoliuk <a_yarmoliuk@apple.com>
2025-04-25 14:27:08 +0100

    Add command to generate shell completion

---
+++ pkl-cli/src/test/kotlin/org/pkl/cli/CliCompletionTest.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright © 2024-2025 Apple Inc. and the Pkl project authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.pkl.cli
+
+import com.github.ajalt.clikt.testing.test
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.pkl.cli.commands.RootCommand
+
+class CliCompletionTest {
+  @Test
+  fun `completion command supports required shells`() {
+    val shellList = listOf("bash", "zsh", "fish")
+
+    for (shell in shellList) {
+      val command = RootCommand()
+      val result = command.test("completion ${shell}")
+
+      assertThat(result.stdout).contains("Command completion for pkl")
+    }
+  }
+}
