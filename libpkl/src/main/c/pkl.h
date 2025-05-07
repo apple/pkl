1e938fbd66c73b0ca170d7f7dc2d562b7e6b6fe9
Kushal Pisavadia <kushal.p@apple.com>
2025-05-07 11:37:20 +0100

    Add doxygen-style comments to `pkl.h` file

--- libpkl/src/main/c/pkl.h
+++ libpkl/src/main/c/pkl.h
@@ -1,7 +1,30 @@
+/**
+* @brief The Pkl Message Response Handler that a user should implement.
+*
+* The resulting messages from `pkl_send` will be sent to this handler using a callback style.
+*/
 typedef void (*PklMessageResponseHandler)(int length, char* message);
 
+/**
+* @brief Initialises and allocates a Pkl executor.
+*
+* @return -1 on failure.
+* @return 0 on success.
+*/
 int pkl_init(PklMessageResponseHandler handler);
 
+/**
+* @brief Send a message to Pkl, providing the length and a pointer to the first byte.
+*
+* @return -1 on failure.
+* @return 0 on success.
+*/
 int pkl_send_message(int length, char* message);
 
+/**
+* @brief Cleans up any resources that were created as part of the `pkl_init` process.
+*
+* @return -1 on failure.
+* @return 0 on success.
+*/
 int pkl_close();
