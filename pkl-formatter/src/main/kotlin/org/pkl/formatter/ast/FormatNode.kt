10e654cfafdddb2e927eaac86a0da014df2c393d
Islon Scherer <i_desouzascherer@apple.com>
2025-05-20 18:01:03 +0200

    Use intermediary formatting tree

---
+++ pkl-formatter/src/main/kotlin/org/pkl/formatter/ast/FormatNode.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright © 2025 Apple Inc. and the Pkl project authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.pkl.formatter.ast
+
+import org.pkl.formatter.Generator
+
+enum class Wrap {
+  ENABLED,
+  DETECT;
+
+  fun isEnabled(): Boolean = this == ENABLED
+}
+
+sealed interface FormatNode {
+  fun width(wrapped: Set<Int>): Int =
+    when (this) {
+      is Nodes -> nodes.sumOf { it.width(wrapped) }
+      is Group -> nodes.sumOf { it.width(wrapped) }
+      is Indent -> nodes.sumOf { it.width(wrapped) }
+      is ForceWrap -> {
+        nodes.sumOf { it.width(wrapped) } + if (nodes.size > 1) Generator.MAX else 0
+      }
+      is IfWrap -> if (wrapped.contains(id)) ifWrap.width(wrapped) else ifNotWrap.width(wrapped)
+      is Text -> text.length
+      is SpaceOrLine -> 1
+      is SemicolonOrLine -> 2
+      else -> 0
+    }
+}
+
+data class Text(val text: String) : FormatNode
+
+object Line : FormatNode
+
+object ForceLine : FormatNode
+
+object SpaceOrLine : FormatNode
+
+object SemicolonOrLine : FormatNode
+
+data class Indent(val nodes: List<FormatNode>) : FormatNode
+
+data class Nodes(val nodes: List<FormatNode>) : FormatNode
+
+data class Group(val id: Int, val nodes: List<FormatNode>) : FormatNode
+
+data class ForceWrap(val id: Int, val nodes: List<FormatNode>) : FormatNode
+
+data class IfWrap(val id: Int, val ifWrap: FormatNode, val ifNotWrap: FormatNode) : FormatNode
