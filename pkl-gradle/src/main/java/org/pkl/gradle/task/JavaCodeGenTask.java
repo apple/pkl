0973774a5d8a23e9e9cb6d8325877059411a2f33
André Rouél <andre.rouel@gmail.com>
2025-07-08 00:31:49 +0200

    Add `@Generated` annotation to generated Java types (#1075)
    JaCoCo automatically excludes methods and classes annotated with @Generated from the coverage reports. This is very important to us as generated code should not normally be included in the coverage report. We want to measure the coverage of the code that we actually wrote and maintain, not the code that was automatically generated by tools.
    
    By introducing a property generatedAnnotation (default value false) one could enable writing @Generated on Java types to be generated.
    
    Co-authored-by: Nullpointer <mike.schulze@tealium.com>

--- pkl-gradle/src/main/java/org/pkl/gradle/task/JavaCodeGenTask.java
+++ pkl-gradle/src/main/java/org/pkl/gradle/task/JavaCodeGenTask.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 Apple Inc. and the Pkl project authors. All rights reserved.
+ * Copyright © 2024-2025 Apple Inc. and the Pkl project authors. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,6 +47,7 @@ public abstract class JavaCodeGenTask extends CodeGenTask {
                 getCliBaseOptions(),
                 getProject().file(getOutputDir()).toPath(),
                 getIndent().get(),
+                getGeneratedAnnotation().get(),
                 getGenerateGetters().get(),
                 getGenerateJavadoc().get(),
                 getGenerateSpringBootConfig().get(),
