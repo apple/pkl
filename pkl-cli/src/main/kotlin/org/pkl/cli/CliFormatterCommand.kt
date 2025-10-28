b424fb65c2065e7657bb758df88bc90e3310651f
Islon Scherer <i_desouzascherer@apple.com>
2025-10-28 15:25:55 +0100

    Coalesce `pkl format` subcommands into the parent command.

--- pkl-cli/src/main/kotlin/org/pkl/cli/CliFormatterCommand.kt
+++ pkl-cli/src/main/kotlin/org/pkl/cli/CliFormatterCommand.kt
@@ -15,40 +15,47 @@
  */
 package org.pkl.cli
 
+import com.github.ajalt.clikt.core.ProgramResult
+import java.io.IOException
 import java.io.Writer
+import java.nio.file.Files
 import java.nio.file.Path
 import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.extension
 import kotlin.io.path.isDirectory
 import kotlin.io.path.name
 import kotlin.io.path.walk
+import kotlin.io.path.writeText
 import org.pkl.commons.cli.CliBaseOptions
 import org.pkl.commons.cli.CliCommand
+import org.pkl.commons.cli.CliException
 import org.pkl.formatter.Formatter
 import org.pkl.formatter.GrammarVersion
 import org.pkl.parser.GenericParserError
 
-abstract class CliFormatterCommand
+class CliFormatterCommand
 @JvmOverloads
 constructor(
-  options: CliBaseOptions,
-  protected val paths: List<Path>,
-  protected val grammarVersion: GrammarVersion,
-  protected val consoleWriter: Writer = System.out.writer(),
-) : CliCommand(options) {
-  protected fun format(file: Path, contents: String): Pair<String, Int> {
-    try {
-      return Formatter().format(contents, grammarVersion) to 0
-    } catch (pe: GenericParserError) {
-      consoleWriter.write("Could not format `$file`: $pe")
-      consoleWriter.appendLine()
-      consoleWriter.flush()
-      return "" to 1
-    }
+  private val paths: List<Path>,
+  private val grammarVersion: GrammarVersion,
+  private val overwrite: Boolean,
+  private val names: Boolean,
+  private val consoleWriter: Writer = System.out.writer(),
+  private val errWriter: Writer = System.err.writer(),
+) : CliCommand(CliBaseOptions()) {
+
+  private fun format(contents: String): String {
+    return Formatter().format(contents, grammarVersion)
+  }
+
+  private fun writeErr(error: String) {
+    errWriter.write(error)
+    errWriter.appendLine()
+    errWriter.flush()
   }
 
   @OptIn(ExperimentalPathApi::class)
-  protected fun paths(): Set<Path> {
+  private fun paths(): Set<Path> {
     val allPaths = mutableSetOf<Path>()
     for (path in paths) {
       if (path.isDirectory()) {
@@ -58,5 +65,51 @@ constructor(
       }
     }
     return allPaths
+  }
+
+  override fun doRun() {
+    val status = Status(SUCCESS)
+
+    for (path in paths()) {
+      try {
+        val contents = Files.readString(path)
+        val formatted = format(contents)
+        if (contents != formatted) {
+          status.update(VALIDATION_ERROR)
+          if (names || overwrite) {
+            // if `--names` or `-w` is specified, only write file names
+            consoleWriter.write(path.toAbsolutePath().toString())
+          } else {
+            consoleWriter.write(formatted)
+          }
+          consoleWriter.appendLine()
+          consoleWriter.flush()
+
+          if (overwrite) {
+            path.writeText(formatted, Charsets.UTF_8)
+          }
+        }
+      } catch (pe: GenericParserError) {
+        writeErr("Could not format `$path`: $pe")
+        status.update(ERROR)
+      } catch (e: IOException) {
+        writeErr("IO error while reading `$path`: ${e.message}")
+        status.update(ERROR)
+      }
+    }
+
+    throw ProgramResult(status.status)
+  }
+
+  companion object {
+    private const val SUCCESS = 0
+    private const val VALIDATION_ERROR = 11
+    private const val ERROR = 1
+
+    private class Status(var status: Int) {
+      fun update(newStatus: Int) {
+        status = if (newStatus == ERROR || newStatus > status) newStatus else status
+      }
+    }
   }
 }
