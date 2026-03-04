0e685591ebe74636bc8c28bdd55119f9f05d6d36
Jen Basch <jbasch@apple.com>
2026-03-03 16:23:42 -0800

    Respect `--omit-project-settings` for all evaluator options (#1459)

--- pkl-commons-cli/src/test/kotlin/org/pkl/commons/cli/CliCommandTest.kt
+++ pkl-commons-cli/src/test/kotlin/org/pkl/commons/cli/CliCommandTest.kt
@@ -16,6 +16,7 @@
 package org.pkl.commons.cli
 
 import com.github.ajalt.clikt.core.parse
+import com.github.ajalt.clikt.parameters.groups.provideDelegate
 import java.net.URI
 import java.nio.file.Path
 import kotlin.io.path.ExperimentalPathApi
@@ -25,25 +26,43 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.io.TempDir
 import org.pkl.commons.cli.commands.BaseCommand
+import org.pkl.commons.cli.commands.ProjectOptions
+import org.pkl.commons.writeString
 import org.pkl.core.SecurityManagers
+import org.pkl.core.evaluatorSettings.TraceMode
+import org.pkl.core.util.IoUtils
 
 @OptIn(ExperimentalPathApi::class)
 class CliCommandTest {
 
-  class CliTest(options: CliBaseOptions) : CliCommand(options) {
-    override fun doRun() = Unit
-
-    val myAllowedResources = allowedResources
-    val myAllowedModules = allowedModules
-    val myResolvedSourceModules = resolvedSourceModules
-  }
-
   private val cmd =
     object : BaseCommand("test", "") {
+      val projectOptions: ProjectOptions by ProjectOptions()
+
       override fun run() = Unit
 
       override val helpString: String = ""
     }
+
+  class CliTest(options: CliBaseOptions) : CliCommand(options) {
+    override fun doRun() = Unit
+
+    val myResolvedSourceModules = resolvedSourceModules
+    val myAllowedModules = allowedModules
+    val myAllowedResources = allowedResources
+    val myRootDir = rootDir
+    val myModulePath = modulePath
+    val myProxyAddress = proxyAddress
+    val myNoProxy = noProxy
+    val myHttpRewrites = httpRewrites
+    val myExternalModuleReaders = externalModuleReaders
+    val myExternalResourceReaders = externalResourceReaders
+
+    fun myEvaluatorBuilder() = evaluatorBuilder()
+
+    @Suppress("PklCliDirectProjectEvaluatorSettingsAccess")
+    val myProjectEvaluatorSettings = project?.evaluatorSettings
+  }
 
   @Test
   fun `--external-resource-reader and --external-module-reader populate allowed modules and resources`() {
@@ -183,5 +202,82 @@ class CliCommandTest {
     val cliTest = CliTest(opts)
     assertThat(cliTest.myResolvedSourceModules)
       .isEqualTo(listOf(tempDir.toUri().resolve("package://example.com/foo@1.2.3#/bar.pkl")))
+  }
+
+  val projectWithAllEvaluatorSettings =
+    """
+    amends "pkl:Project"
+    
+    evaluatorSettings {
+      externalProperties { ["foo"] = "bar" }
+      env { ["foo"] = "bar" }
+      allowedModules { "file:" }
+      allowedResources { "file:" }
+      color = "always"
+      noCache = true
+      modulePath { "/tmp/modulepath" }
+      timeout = 30.s
+      moduleCacheDir = "/tmp/cache"
+      rootDir = "/tmp/root"
+      http {
+        proxy {
+          address = "http://example.com:80"
+          noProxy { "example.com" }
+        }
+        rewrites {
+          ["https://example.com/foo/"] = "https://example.com/bar/"
+        }
+      }
+      externalModuleReaders {
+        ["foo"] { executable = "foo" }
+      }
+      externalResourceReaders {
+        ["foo"] { executable = "foo" }
+      }
+      traceMode = "pretty"
+    }
+  """
+      .trimIndent()
+
+  @Test
+  fun `test that --omit-project-settings actually omits project settings`(@TempDir tempDir: Path) {
+    val project = tempDir.resolve("PklProject").writeString(projectWithAllEvaluatorSettings)
+    cmd.parse(arrayOf("--working-dir=$tempDir", "--omit-project-settings"))
+    val opts =
+      cmd.baseOptions.baseOptions(listOf(project.toUri()), cmd.projectOptions, testMode = true)
+    val cliTest = CliTest(opts)
+    val builder = cliTest.myEvaluatorBuilder()
+    assertThat(cliTest.myAllowedModules).isEqualTo(SecurityManagers.defaultAllowedModules)
+    assertThat(cliTest.myAllowedResources).isEqualTo(SecurityManagers.defaultAllowedResources)
+    assertThat(cliTest.myRootDir).isNull()
+    assertThat(builder.environmentVariables).isEqualTo(System.getenv())
+    assertThat(builder.externalProperties).isEmpty()
+    assertThat(builder.moduleCacheDir).isEqualTo(IoUtils.getDefaultModuleCacheDir())
+    assertThat(cliTest.myModulePath).isEmpty()
+    assertThat(builder.color).isFalse
+    assertThat(cliTest.myProxyAddress).isNull()
+    assertThat(cliTest.myNoProxy).isNull()
+    assertThat(cliTest.myHttpRewrites).isNull()
+    assertThat(cliTest.myExternalModuleReaders).isEmpty()
+    assertThat(cliTest.myExternalResourceReaders).isEmpty()
+    assertThat(builder.traceMode).isEqualTo(TraceMode.COMPACT)
+  }
+
+  // hygiene test to ensure new evaluator settings get covered by the above test
+  @Test
+  fun `test project sets all evaluator settings`(@TempDir tempDir: Path) {
+    val project = tempDir.resolve("PklProject").writeString(projectWithAllEvaluatorSettings)
+    cmd.parse(arrayOf("--working-dir=$tempDir"))
+    val opts = cmd.baseOptions.baseOptions(listOf(project.toUri()), testMode = true)
+    val cliTest = CliTest(opts)
+    cliTest.myProjectEvaluatorSettings
+      ?.javaClass
+      ?.declaredMethods
+      ?.filter { it.parameterCount == 0 }
+      ?.forEach {
+        assertThat(it.invoke(cliTest.myProjectEvaluatorSettings))
+          .overridingErrorMessage("project evaluator settings returned null for ${it.name}")
+          .isNotNull
+      }
   }
 }
