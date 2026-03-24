b1fa8bea32ae91401e7173dee7fbfa546a9147a8
Jen Basch <jbasch@apple.com>
2026-03-24 12:43:44 -0700

    Allow custom/external resources to be "not found" (#1471)
    This allows custom/external resources to produce `null` values for
    nullable reads (`read?`)
    
    Ref: https://github.com/apple/pkl-go/issues/157

--- pkl-core/src/main/java/org/pkl/core/externalreader/ExternalReaderProcessImpl.java
+++ pkl-core/src/main/java/org/pkl/core/externalreader/ExternalReaderProcessImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024-2025 Apple Inc. and the Pkl project authors. All rights reserved.
+ * Copyright © 2024-2026 Apple Inc. and the Pkl project authors. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -220,7 +220,6 @@ final class ExternalReaderProcessImpl implements ExternalReaderProcess {
                     .send(
                         request,
                         (response) -> {
-                          log(response.toString());
                           if (response instanceof InitializeResourceReaderResponse resp) {
                             var spec =
                                 resp.spec() == null
