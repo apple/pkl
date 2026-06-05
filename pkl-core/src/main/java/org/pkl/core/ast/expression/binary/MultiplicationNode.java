01f8fcae7b5000d4a01635257ec35ce21eb71eaa
Daniel Chao <dan.chao@apple.com>
2026-06-05 12:55:27 -0700

    Catch correct exception type in MultiplicationNode (#1652)
    Closes #1651

--- pkl-core/src/main/java/org/pkl/core/ast/expression/binary/MultiplicationNode.java
+++ pkl-core/src/main/java/org/pkl/core/ast/expression/binary/MultiplicationNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 Apple Inc. and the Pkl project authors. All rights reserved.
+ * Copyright © 2024-2026 Apple Inc. and the Pkl project authors. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,7 +31,7 @@ public abstract class MultiplicationNode extends BinaryExpressionNode {
   protected long eval(long left, long right) {
     try {
       return StrictMath.multiplyExact(left, right);
-    } catch (VmException e) {
+    } catch (ArithmeticException e) {
       CompilerDirectives.transferToInterpreter();
       throw exceptionBuilder().evalError("integerOverflow").build();
     }
