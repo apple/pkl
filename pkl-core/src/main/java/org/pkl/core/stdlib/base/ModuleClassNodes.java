a41543cdc39946775945c2a42b10ea79da5e13c9
Jen Basch <jbasch@apple.com>
2026-07-14 14:06:55 -0700

    s/descendent/descendant

--- pkl-core/src/main/java/org/pkl/core/stdlib/base/ModuleClassNodes.java
+++ pkl-core/src/main/java/org/pkl/core/stdlib/base/ModuleClassNodes.java
@@ -55,7 +55,7 @@ public final class ModuleClassNodes {
       }
 
       throw exceptionBuilder()
-          .evalError("noDescendentPathBetweenModules", selfUri, otherUri)
+          .evalError("noDescendantPathBetweenModules", selfUri, otherUri)
           .build();
     }
   }
