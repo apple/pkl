292a33808ca469e6b58a9832acdbd02e2cb5970d
henriquejsza <henriquejsza@gmail.com>
2026-08-28 22:09:17 -0300

    Improve error for amendments without defaults
    Report the missing default value only when an object body is trying to amend an implicitly defaulted property. Preserve undefined-value errors from explicit property bodies.

--- pkl-core/src/main/java/org/pkl/core/ast/expression/member/ReadSuperPropertyNode.java
+++ pkl-core/src/main/java/org/pkl/core/ast/expression/member/ReadSuperPropertyNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 Apple Inc. and the Pkl project authors. All rights reserved.
+ * Copyright © 2024-2026 Apple Inc. and the Pkl project authors. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,10 +16,13 @@
 package org.pkl.core.ast.expression.member;
 
 import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.IndirectCallNode;
 import com.oracle.truffle.api.source.SourceSection;
 import org.pkl.core.ast.ExpressionNode;
+import org.pkl.core.ast.member.DefaultPropertyBodyNode;
+import org.pkl.core.ast.member.ObjectMember;
 import org.pkl.core.runtime.*;
 
 public final class ReadSuperPropertyNode extends ExpressionNode {
@@ -27,13 +30,24 @@ public final class ReadSuperPropertyNode extends ExpressionNode {
   private final Identifier propertyName;
   private final boolean needsConst;
 
+  private final boolean isPropertyAmendment;
+
   @Child private IndirectCallNode callNode = IndirectCallNode.create();
 
   public ReadSuperPropertyNode(
       SourceSection sourceSection, Identifier propertyName, boolean needsConst) {
+    this(sourceSection, propertyName, needsConst, false);
+  }
+
+  public ReadSuperPropertyNode(
+      SourceSection sourceSection,
+      Identifier propertyName,
+      boolean needsConst,
+      boolean isPropertyAmendment) {
     super(sourceSection);
     this.propertyName = propertyName;
     this.needsConst = needsConst;
+    this.isPropertyAmendment = isPropertyAmendment;
   }
 
   // TODO: how can this be optimized?
@@ -64,16 +78,39 @@ public final class ReadSuperPropertyNode extends ExpressionNode {
       if (constantValue != null) return constantValue; // TODO: type check
 
       // caching the result of a super call is tricky (function of both receiver and owner)
-      return callNode.call(
-          property.getCallTarget(),
-          // TODO: should the marker only turn off constraint checking, not overall type checking?
-          receiver,
-          owner,
-          propertyName,
-          VmUtils.SKIP_TYPECHECK_MARKER);
+      try {
+        return callNode.call(
+            property.getCallTarget(),
+            // TODO: should the marker only turn off constraint checking, not overall type checking?
+            receiver,
+            owner,
+            propertyName,
+            VmUtils.SKIP_TYPECHECK_MARKER);
+      } catch (VmUndefinedValueException e) {
+        if (!isPropertyAmendment) throw e;
+        CompilerDirectives.transferToInterpreter();
+        throw convertUndefinedAmendment(property, e);
+      }
     }
 
     // TODO: refine when to return VmDynamic.empty() and when to fail
     return VmDynamic.empty();
+  }
+
+  @TruffleBoundary
+  private VmException convertUndefinedAmendment(
+      ObjectMember property, VmUndefinedValueException original) {
+    var memberNode = property.getMemberNode();
+    if (memberNode == null
+        || !(memberNode.getBodyNode() instanceof DefaultPropertyBodyNode defaultBody)) {
+      return original;
+    }
+    var typeNode = defaultBody.getTypeNode();
+    if (typeNode == null) return original;
+    return exceptionBuilder()
+        .evalError(
+            "cannotInstantiateTypeWithoutDefault", typeNode.getSourceSection().getCharacters())
+        .withSourceSection(typeNode.getSourceSection())
+        .build();
   }
 }
